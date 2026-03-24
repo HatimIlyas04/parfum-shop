8b3c9942bd59a23abdf896e0dd1ca1ac235e91f5
Hatim Ilyas <hatimilyass766@gmail.com>
2026-03-24 11:07:31 +0100

    fix db connection for render

--- backend/server.js
+++ backend/server.js
@@ -6,11 +6,18 @@ const app = express();
 app.use(cors());
 app.use(express.json());
 
+require("dotenv").config();
+const fs = require("fs");
+
 const db = mysql.createConnection({
-  host: "localhost",
-  user: "root",
-  password: "ZA3TERza3ter",
-  database: "parfum_shop",
+  host: process.env.DB_HOST,
+  user: process.env.DB_USER,
+  password: process.env.DB_PASS,
+  database: process.env.DB_NAME,
+  port: process.env.DB_PORT,
+  ssl: {
+    ca: fs.readFileSync("./ca.pem")
+  }
 });
 
 // Test database connection
@@ -210,4 +217,5 @@ app.get("/orders/:id", (req, res) => {
   });
 });
 
-app.listen(5000, () => console.log("✅ Server running on port 5000"));
+const PORT = process.env.PORT || 5000;
+app.listen(PORT, () => console.log("Server running on port", PORT));
