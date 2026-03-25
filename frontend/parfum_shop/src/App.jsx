657f261fbf62a4bfc99a3c4cb68bd0730c7abcb7
Hatim Ilyas <hatimilyass766@gmail.com>
2026-03-25 14:37:56 +0100

    Ajout page Notre Histoire

--- frontend/parfum_shop/src/App.jsx
+++ frontend/parfum_shop/src/App.jsx
@@ -5,7 +5,7 @@ import Cart from "./pages/Cart";
 import ProductDetails from "./pages/ProductDetails";
 import Admin from "./pages/Admin";
 import Checkout from "./pages/Checkout";
-import NotreHistoire from "./pages/NotreHistoire";
+import NotreHistoire from "./pages/NotreHistoire1";
 
 
 function App() {
